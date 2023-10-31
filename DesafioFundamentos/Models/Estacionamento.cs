bb2bc6586254f434f93a9cc2f05dd19de5ac0c5b
kevincrz <kevingcruz99@gmail.com>
2023-10-31 10:42:54 -0300

    Testando conexão no gitHub

--- DesafioFundamentos/Models/Estacionamento.cs
+++ DesafioFundamentos/Models/Estacionamento.cs
@@ -14,6 +14,7 @@ namespace DesafioFundamentos.Models
 
         public void AdicionarVeiculo()
         {
+            //TESTANDO CONEXÃO GIT
             // TODO: Pedir para o usuário digitar uma placa (ReadLine) e adicionar na lista "veiculos"
             // *IMPLEMENTE AQUI*
             Console.WriteLine("Digite a placa do veículo para estacionar:");
